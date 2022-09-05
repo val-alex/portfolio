cfb86609baabed357b184b7e9a4bd2a4ad670f4f
Val <valentin.m.alexeev@gmail.com>
2022-09-05 16:52:01 +0300

    updated theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,26 +2,42 @@ import {
   extendTheme,
   theme as base
 } from '@chakra-ui/react';
+import { mode } from "@chakra-ui/theme-tools";
+import { ButtonStyles as Button } from "./buttonStyles";
 
 const theme = extendTheme(
   {
     fonts: {
       heading: `'Roboto', ${base.fonts.heading}`,
       body: `'Roboto', ${base.fonts.body}`,
+      mono: `'Roboto', ${base.fonts.body}`,
     },
     colors: {
+      stroke: "#001e1d",
+      main: "#e8e4e6",
+      highlight: "#f9bc60",
+      secondary: "#abd1c6",
+      tertiary: "#e16162",
       brand: {
-        50: '#f5fee5',
-        100: '#e1fbb2',
-        200: '#cdf781',
-        300: '#b8ee56',
-        400: '#a2e032',
-        500: '#8ac919',
-        600: '#71ab09',
-        700: '#578602',
-        800: '#3c5e00',
-        900: '#203300',
+        50: '#fffffe',
+        100: '#e8e4e6',
+        200: '#abd1c6',
+        300: '#f9bc60',
+        400: '#e16162',
+        500: '#004643',
+        600: '#0f3433',
+        700: '#001e1d',
       },
+    },
+    components: {
+      Button,
+    },
+    styles: {
+      global: (props: any): any => ({
+        body: {
+          bg: mode('brand.200', 'brand.500')(props),
+        },
+      }),
     },
   },
 );
