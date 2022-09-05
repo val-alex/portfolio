bbe8ef719c73ab08bc84c0f2e63060848040d41d
Val <valentin.m.alexeev@gmail.com>
2022-09-05 22:32:52 +0300

    updated contact form

--- src/components/contactForm.tsx
+++ src/components/contactForm.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable react-hooks/rules-of-hooks */ // TODO: fix this later
 import {
   Box,
   Button,
+  Center,
   FormControl,
   FormLabel,
   Input,
@@ -10,29 +12,103 @@ import {
   Textarea,
   useColorModeValue,
 } from "@chakra-ui/react";
+import { useForm, ValidationError } from "@formspree/react";
 import { Mail, User } from "react-feather";
 
 export const ContactForm = () => {
+  const [state, handleSubmit] = useForm(
+    `${process.env.NEXT_PUBLIC_FORMSPREE_API_KEY}`
+  );
+
+  if (state.succeeded) {
+    return (
+      <Center>
+        Thank you for sending me an email! Will get in touch soon :)
+      </Center>
+    );
+  }
+
   return (
     <Box
-      bg={useColorModeValue("brand.500", "main")}
-      borderRadius="lg"
+      borderRadius={"lg"}
       p={8}
+      shadow={"base"}
+      bg={useColorModeValue("brand.500", "main")}
       color={useColorModeValue("main", "stroke")}
-      shadow="base"
     >
-      <Stack spacing={5}>
-        <FormControl isRequired>
-          <FormLabel>Name</FormLabel>
+      <form onSubmit={handleSubmit}>
+        <Stack spacing={5}>
+          <FormControl isRequired>
+            <FormLabel htmlFor="name">Name</FormLabel>
 
-          <InputGroup>
-            <InputLeftElement>
-              <User />
-            </InputLeftElement>
-            <Input
-              type="text"
-              name="name"
-              placeholder="Your Name"
+            <InputGroup>
+              <InputLeftElement>
+                <User />
+              </InputLeftElement>
+              <Input
+                id="name"
+                type="text"
+                name="name"
+                placeholder="Your Name"
+                _placeholder={{ color: useColorModeValue("main", "stroke") }}
+                borderColor={useColorModeValue("main", "stroke")}
+                _hover={{
+                  borderColor: useColorModeValue("highlight", "highlight"),
+                }}
+                _focusVisible={{
+                  zIndex: 1,
+                  borderColor: useColorModeValue("highlight", "highlight"),
+                  boxShadow: "0 0 0 1px #f9bc60",
+                }}
+              />
+              <ValidationError
+                prefix="Name"
+                field="name"
+                errors={state.errors}
+              />
+            </InputGroup>
+          </FormControl>
+
+          <FormControl isRequired>
+            <FormLabel htmlFor="email">Email</FormLabel>
+
+            <InputGroup>
+              <InputLeftElement>
+                <Mail />
+              </InputLeftElement>
+              <Input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Your Email"
+                _placeholder={{ color: useColorModeValue("main", "stroke") }}
+                borderColor={useColorModeValue("main", "stroke")}
+                _hover={{
+                  borderColor: useColorModeValue("highlight", "highlight"),
+                }}
+                _focusVisible={{
+                  zIndex: 1,
+                  borderColor: useColorModeValue("highlight", "highlight"),
+                  boxShadow: "0 0 0 1px #f9bc60",
+                }}
+              />
+              <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+              />
+            </InputGroup>
+          </FormControl>
+
+          <FormControl isRequired>
+            <FormLabel htmlFor="message">Message</FormLabel>
+
+            <Textarea
+              id="message"
+              name="message"
+              rows={6}
+              resize="none"
+              placeholder="Your Message"
               _placeholder={{ color: useColorModeValue("main", "stroke") }}
               borderColor={useColorModeValue("main", "stroke")}
               _hover={{
@@ -44,64 +120,25 @@ export const ContactForm = () => {
                 boxShadow: "0 0 0 1px #f9bc60",
               }}
             />
-          </InputGroup>
-        </FormControl>
-
-        <FormControl isRequired>
-          <FormLabel>Email</FormLabel>
-
-          <InputGroup>
-            <InputLeftElement>
-              <Mail />
-            </InputLeftElement>
-            <Input
-              type="email"
-              name="email"
-              placeholder="Your Email"
-              _placeholder={{ color: useColorModeValue("main", "stroke") }}
-              borderColor={useColorModeValue("main", "stroke")}
-              _hover={{
-                borderColor: useColorModeValue("highlight", "highlight"),
-              }}
-              _focusVisible={{
-                zIndex: 1,
-                borderColor: useColorModeValue("highlight", "highlight"),
-                boxShadow: "0 0 0 1px #f9bc60",
-              }}
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
             />
-          </InputGroup>
-        </FormControl>
+          </FormControl>
 
-        <FormControl isRequired>
-          <FormLabel>Message</FormLabel>
-
-          <Textarea
-            name="message"
-            rows={6}
-            resize="none"
-            placeholder="Your Message"
-            _placeholder={{ color: useColorModeValue("main", "stroke") }}
-            borderColor={useColorModeValue("main", "stroke")}
+          <Button
+            variant="highlight"
             _hover={{
-              borderColor: useColorModeValue("highlight", "highlight"),
+              bg: useColorModeValue("main", "secondary"),
             }}
-            _focusVisible={{
-              zIndex: 1,
-              borderColor: useColorModeValue("highlight", "highlight"),
-              boxShadow: "0 0 0 1px #f9bc60",
-            }}
-          />
-        </FormControl>
-
-        <Button
-          variant="highlight"
-          _hover={{
-            bg: "main",
-          }}
-        >
-          Send Message
-        </Button>
-      </Stack>
+            type="submit"
+            disabled={state.submitting}
+          >
+            Send Message
+          </Button>
+        </Stack>
+      </form>
     </Box>
   );
 };
