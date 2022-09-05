855cc42200143f67db4a14233164df363feb5830
Val <valentin.m.alexeev@gmail.com>
2022-09-05 16:57:14 +0300

    added work section

---
+++ src/components/works.tsx
@@ -0,0 +1,34 @@
+import { Heading, ListItem, UnorderedList } from "@chakra-ui/react";
+
+export const Works = () => {
+  return (
+    <>
+      <Heading as="h3" variant="section-title" id="works">
+        Works
+      </Heading>
+
+      <UnorderedList>
+        <ListItem>
+          Senior Front-End Web Developer (React.js) - <strong>AKQA</strong> -
+          (May 2021 - Oct 2021 )
+        </ListItem>
+        <ListItem>
+          Senior Front-End Web Developer (React.js) - <strong>Codebase8</strong>{" "}
+          - (Jan 2021 - Apr 2021)
+        </ListItem>
+        <ListItem>
+          Senior Front-End Web Developer (React.js) -{" "}
+          <strong>The Frameworks</strong> - (Apr 2020 - Dec 2020)
+        </ListItem>
+        <ListItem>
+          Front-End Web Developer (React.js) - <strong>Loveholidays</strong> -
+          (Apr 2018 - March 2020)
+        </ListItem>
+        <ListItem>
+          Front-End Web Developer (React.js) - <strong>Shelter</strong> - (May
+          2017 - Dec 2017)
+        </ListItem>
+      </UnorderedList>
+    </>
+  );
+};
